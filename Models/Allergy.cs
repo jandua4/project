c65ebb77d5a6849a784626687089575560612d0f
Aman-Rai Jandu <jandua4@cu.coventry.ac.uk>
2021-12-02 13:08:49 +0000

    Added Allergy and Restaurant Models, and added Scaffolded Identity

---
+++ Models/Allergy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Allergy
+    {
+        public int AllergyID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DbSet<Allergy> Allergy { get; set; }
+
+        // Menu Filepath. Should be nullable
+        public string Filename { get; set; }
+    }
+}
